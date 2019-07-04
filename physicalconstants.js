ed00a7d852a6eb99a02adfc595ad48698b692290
tomdavsmi <39323451+tomdavsmi@users.noreply.github.com>
2019-07-04 09:31:27 +0100

    Update physicalconstants.js

--- physicalconstants.js
+++ physicalconstants.js
@@ -16,50 +16,60 @@ Numbas.addExtension('physicalconstants',['math','jme'],function(physicalconstant
    		},{unwrapValues: true});
 	var physical_constants = [
 		{
-			"name":"faraday", 
-			"value": 96485.33212, 	
-			"units":"C*mol^-1"
+			"name": "protoncharge", "value": 1.6022E-19, "units": "C"
 		},
 		{
-			"name":"avogadro", 
-			"value": 6.02214076E23,
-			"units": "mol^-1"
+			"name": "electron-rest-mass", "value": 9.1094E-31, "units": "kg"
+		},
+		{
+			"name": "proton-rest-mass", "value": 1.6726E-27, "units": "kg"
+		},
+		{
+			"name": "atomic-mass-constant", "value": 1.6605E-27, "units": "kg"
+		},
+		{
+			"name": "speed-light-vacuum", "value": 2.9979E8, "units": "m s^-1"
+		},
+		{
+			"name": "planck", "value":6.6261E-34, "units": "J s"
+		},
+		{
+			"name": "reduced-planck", "value": 1.0546E-34, "units": "J s"
+		},
+		{
+			"name": "boltzmann", "value": 1.3807E-23, "units": "J K^-1"
+		},
+		{
+			"name": "avogadro", "value": 3.0221E23, "units": "mol^-1"
+		},
+		{
+			"name": "gas-constant", "value": 8.3145, "units": "J K^-1 mol^-1"
 		},
 		{	
-			"name":"boltzmann", 
-			"value": 1.380649E-23, 
-			"units":"J K^-1"		
+			"name": "ice-point-temperature", "value": 273.15, "units": "K"
 		},
 		{
-			"name":"molargas", 
-			"value":8.314462, 
-			"units": "J mol^-1 K^-1"
+			"name": "faraday", "value": 9.6485E4, "units": "C mol^-1"
 		},
 		{
-			"name":"planck", 
-			"value":6.62607015E-34, 
-			"units":"J Hz^-1"
-		},
-		{		
-			"name":"reducedplanck",
-			"value":1.054571817E-34,
-			"units":"J s"
-		{
-			"name":"speedoflight", 
-			"value":299792458, 
-			"units":"m s^-1"
+			"name": "vacuum-permeability", "value": 1.256637E-6, "units": "kg m s^-2 A-2"
 		},
 		{
-			"name":"elementarycharge",
-			"value":1.602176634E-19,
-			"units":"C"
+			"name": "vacuum-permittivity", "value": 8.8542E-12, "units": "F m^-1"
 		},
 		{
-			"name":"vacuumelectricpermittivity",
-			"value":8.8541878128(13)E-12,
-			"units":"F m^-1"
+			"name": "bohr-radius", "value": 52.9, "units": "pm"
+		},
+		{
+			"name": "bohr-magneton", "value": 9.2740E-24, "units": "A m^-2"
+		},
+		{
+			"name": "rydberg-constant", "value": 1.097373E5, "units": "cm^-1"
+		},
+		{
+			"name": "hartree-energy", "value": 4.3598E-18, "units": "J"
 		}
-	];
+	]
 });
 
 
